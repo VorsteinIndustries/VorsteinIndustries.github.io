a93f3f8fa3f8dc87e0141c76907811963ba561e5
CyrusVorwald <vgt085@gmail.com>
2015-09-27 19:02:29 -0400

    cleaner look

--- js/script.js
+++ js/script.js
@@ -21,30 +21,11 @@ $(document).ready(function(){
 	
 
 	$nav_bar.hover(function() {
-    	$nav_menu.stop().animate({ "font-size": "23px" }, 500);
+    	$nav_menu.stop().animate({ "font-size": "30px" }, 500);
     	$nav_bar.stop().animate({ "height": "45px" }, 500);
 	}, function() {
-    	$nav_menu.stop().animate({ "font-size": "22px" }, 500);
+    	$nav_menu.stop().animate({ "font-size": "28px" }, 500);
     	$nav_bar.stop().animate({ "height": "40px" }, 500);
-	});
-
-	$(window).scroll(function() {
-		$scrolling = true;
-	    clearTimeout($.data(this, 'scrollTimer'));
-	    $.data(this, 'scrollTimer', setTimeout(function() {
-        	$scrolling = false;
-        	console.log("hasn't scrolled for 600 ms");
-        	$nav_menu.stop().animate({ "font-size": "22px" }, 500);
-    		$nav_bar.stop().animate({ "height": "40px" }, 500);
-    	}, 600));
-    	if ($scrolling == true){
-    		console.log("is scrolling");
-    		$nav_menu.stop().animate({ "font-size": "21px" }, 500);
-    		$nav_bar.stop().animate({ "height": "38px" }, 500);
-    	} else {
-    		console.log("is not scrolling");
-    		
-    	}
 	});
 
 	var $bob_img  = $('#cyrus');
