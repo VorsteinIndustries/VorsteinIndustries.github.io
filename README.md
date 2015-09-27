99c05ba13d2b37d03a2faf1905a472565a80b35d
CyrusVorwald <vgt085@gmail.com>
2015-09-27 15:21:19 -0400

    works with more resolutions

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 9/21/2015
   Slight installation problems, but we gucci now mayne.
+9/22
+  Changed background, organized layout a bit. Changed font. Cleaned up css slightly. 
+  
+TODO 9/23
+  Add actual bios and information. Add pictures. Add auto scroll function to bio.
--- js/script.js
+++ js/script.js
@@ -1,40 +1,80 @@
 
 
 $(document).ready(function(){
+	
+	var nav_menu_shown = true;
+	var $nav_menu = $("#categories");
+	var $nav_bar = $(".nav_bar")
+	var $scrolling = false;
+	var screenHeight = screen.height;
+	
+	if (screenHeight < 800) {
+	  $('body').css('zoom', 0.8);
+	} else {
+	     $('body').css('zoom', 1);
+	}
 
-	var $bob_info = $('#bob_info');
-	var $bob_img  = $('#bob');
+	$nav_bar.hover(function() {
+    	$nav_menu.stop().animate({ "font-size": "17pt" }, 500);
+    	$nav_bar.stop().animate({ "height": "5.5%" }, 500);
+	}, function() {
+    	$nav_menu.stop().animate({ "font-size": "16pt" }, 500);
+    	$nav_bar.stop().animate({ "height": "5%" }, 500);
+	});
+
+	$(window).scroll(function() {
+		$scrolling = true;
+	    clearTimeout($.data(this, 'scrollTimer'));
+	    $.data(this, 'scrollTimer', setTimeout(function() {
+        	$scrolling = false;
+        	console.log("hasn't scrolled for 600 ms");
+        	$nav_menu.stop().animate({ "font-size": "16pt" }, 500);
+    		$nav_bar.stop().animate({ "height": "5%" }, 500);
+    	}, 200));
+    	if ($scrolling == true){
+    		console.log("is scrolling");
+    		$nav_menu.stop().animate({ "font-size": "15pt" }, 500);
+    		$nav_bar.stop().animate({ "height": "4.5%" }, 500);
+    	} else {
+    		console.log("is not scrolling");
+    	}
+	});
+
+	var $bob_img  = $('#cyrus');
+	var $bob_info = $('#cyrus_info');
+	
 	var is_shown_bob  = false;
 	$bob_img.click(function(){
-		if (!is_shown_bob ) {
-			
-			$bob_info.slideDown();
+		if (!is_shown_bob ) {	
+			$bob_info.fadeIn('200');
+			window.scrollTo(0, 800);
 			is_shown_bob  = true;
 			if(is_shown_sponge) {
-				$sponge_info.slideUp();
+				$sponge_info.hide();
 				is_shown_sponge  = false;
 			}
 		} else {
-			$bob_info.slideUp();
+			$bob_info.hide();
 			is_shown_bob  = false;
 		}
 		
 	});
 	
-	var $sponge_img  = $('#sponge');
-	var $sponge_info = $('#sponge_info');
+	var $sponge_img  = $('#greg');
+	var $sponge_info = $('#greg_info');
 	var is_shown_sponge  = false;
 	$sponge_img.click(function(){
 		if (!is_shown_sponge ) {
 			
-			$sponge_info.slideDown();
+			$sponge_info.fadeIn('200');
+			window.scrollTo(0, 800);
 			is_shown_sponge  = true;
 			if (is_shown_bob) {
-				$bob_info.slideUp();
+				$bob_info.hide();
 				is_shown_bob  = false;
 			}
 		} else {
-			$sponge_info.slideUp();
+			$sponge_info.hide();
 			is_shown_sponge  = false;
 		}
 		
